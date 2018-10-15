5bea8b0908a1e359f87ebfb1d9ab7ec2f9fe4482
Tuomo Jauhiainen <juppi@project-apollo.co.uk>
2018-10-15 20:16:59 +0300

    Create a header with forward declarations of most common engine types.

--- engine/core/defines.h
+++ engine/core/defines.h
@@ -6,6 +6,7 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <assert.h>
+#include "core/types.h"
 
 #ifdef _WIN32
 #define INLINE __inline
---
+++ engine/core/types.h
@@ -0,0 +1,26 @@
+#pragma once
+#ifndef __TYPES_H
+#define __TYPES_H
+
+/*
+====================================================================================================
+
+	Types
+
+	Contains forward declarations of most common engine types.
+
+====================================================================================================
+*/
+
+// Rendering and models
+typedef struct mesh_t mesh_t;
+typedef struct model_t model_t;
+typedef struct shader_t shader_t;
+typedef struct texture_t texture_t;
+
+// Scene hierarchy
+typedef struct camera_t camera_t;
+typedef struct object_t object_t;
+typedef struct scene_t scene_t;
+
+#endif
