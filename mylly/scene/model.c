f11671b7c3c5f420d4e56d2e0c1c216d5b9cb7dc
Tuomo Jauhiainen <juppi@project-apollo.co.uk>
2019-07-19 14:12:03 +0300

    Add normals for cube primitive.

--- mylly/scene/model.c
+++ mylly/scene/model.c
@@ -27,10 +27,10 @@ static vindex_t quad_indices[] = {
 static vertex_t cube_vertices[] = {
 
 	// Side vertices
-	{ .pos = { .vec = { -0.5f, -0.5f, -0.5f } }, .normal = { .vec = { 0, 0, 1 } }, .uv = { .vec = { 0, 0 } } },
-	{ .pos = { .vec = { 0.5f, -0.5f, -0.5f } }, .normal = { .vec = { 0, 0, 1 } }, .uv = { .vec = { 1, 0 } } },
-	{ .pos = { .vec = { -0.5f, 0.5f, -0.5f } }, .normal = { .vec = { 0, 0, 1 } }, .uv = { .vec = { 0, 1 } } },
-	{ .pos = { .vec = { 0.5f, 0.5f, -0.5f } }, .normal = { .vec = { 0, 0, 1 } }, .uv = { .vec = { 1, 1 } } },
+	{ .pos = { .vec = { -0.5f, -0.5f, -0.5f } }, .normal = { .vec = { 0, 0, -1 } }, .uv = { .vec = { 0, 0 } } },
+	{ .pos = { .vec = { 0.5f, -0.5f, -0.5f } }, .normal = { .vec = { 0, 0, -1 } }, .uv = { .vec = { 1, 0 } } },
+	{ .pos = { .vec = { -0.5f, 0.5f, -0.5f } }, .normal = { .vec = { 0, 0, -1 } }, .uv = { .vec = { 0, 1 } } },
+	{ .pos = { .vec = { 0.5f, 0.5f, -0.5f } }, .normal = { .vec = { 0, 0, -1 } }, .uv = { .vec = { 1, 1 } } },
 
 	{ .pos = { .vec = { -0.5f, -0.5f, 0.5f } }, .normal = { .vec = { 0, 0, 1 } }, .uv = { .vec = { 1, 0 } } },
 	{ .pos = { .vec = { 0.5f, -0.5f, 0.5f } }, .normal = { .vec = { 0, 0, 1 } }, .uv = { .vec = { 0, 0 } } },
@@ -38,26 +38,36 @@ static vertex_t cube_vertices[] = {
 	{ .pos = { .vec = { 0.5f, 0.5f, 0.5f } }, .normal = { .vec = { 0, 0, 1 } }, .uv = { .vec = { 0, 1 } } },
 
 	// Bottom vertices
-	{ .pos = { .vec = { -0.5f, -0.5f, -0.5f } }, .normal = { .vec = { 0, 0, 1 } }, .uv = { .vec = { 0, 0 } } },
-	{ .pos = { .vec = { 0.5f, -0.5f, -0.5f } }, .normal = { .vec = { 0, 0, 1 } }, .uv = { .vec = { 1, 0 } } },
-	{ .pos = { .vec = { -0.5f, -0.5f, 0.5f } }, .normal = { .vec = { 0, 0, 1 } }, .uv = { .vec = { 0, 1 } } },
-	{ .pos = { .vec = { 0.5f, -0.5f, 0.5f } }, .normal = { .vec = { 0, 0, 1 } }, .uv = { .vec = { 1, 1 } } },
+	{ .pos = { .vec = { -0.5f, -0.5f, -0.5f } }, .normal = { .vec = { 0, -1, 0 } }, .uv = { .vec = { 0, 0 } } },
+	{ .pos = { .vec = { 0.5f, -0.5f, -0.5f } }, .normal = { .vec = { 0, -1, 0 } }, .uv = { .vec = { 1, 0 } } },
+	{ .pos = { .vec = { -0.5f, -0.5f, 0.5f } }, .normal = { .vec = { 0, -1, 0 } }, .uv = { .vec = { 0, 1 } } },
+	{ .pos = { .vec = { 0.5f, -0.5f, 0.5f } }, .normal = { .vec = { 0, -1, 0 } }, .uv = { .vec = { 1, 1 } } },
 
 	// Top vertices
-	{ .pos = { .vec = { -0.5f, 0.5f, -0.5f } }, .normal = { .vec = { 0, 0, 1 } }, .uv = { .vec = { 0, 0 } } },
-	{ .pos = { .vec = { 0.5f, 0.5f, -0.5f } }, .normal = { .vec = { 0, 0, 1 } }, .uv = { .vec = { 1, 0 } } },
-	{ .pos = { .vec = { -0.5f, 0.5f, 0.5f } }, .normal = { .vec = { 0, 0, 1 } }, .uv = { .vec = { 0, 1 } } },
-	{ .pos = { .vec = { 0.5f, 0.5f, 0.5f } }, .normal = { .vec = { 0, 0, 1 } }, .uv = { .vec = { 1, 1 } } },
+	{ .pos = { .vec = { -0.5f, 0.5f, -0.5f } }, .normal = { .vec = { 0, 1, 0 } }, .uv = { .vec = { 0, 0 } } },
+	{ .pos = { .vec = { 0.5f, 0.5f, -0.5f } }, .normal = { .vec = { 0, 1, 0 } }, .uv = { .vec = { 1, 0 } } },
+	{ .pos = { .vec = { -0.5f, 0.5f, 0.5f } }, .normal = { .vec = { 0, 1, 0 } }, .uv = { .vec = { 0, 1 } } },
+	{ .pos = { .vec = { 0.5f, 0.5f, 0.5f } }, .normal = { .vec = { 0, 1, 0 } }, .uv = { .vec = { 1, 1 } } },
 
+	// Other side vertices
+	{ .pos = { .vec = { 0.5f, -0.5f, -0.5f } }, .normal = { .vec = { 1, 0, 0 } }, .uv = { .vec = { 1, 0 } } },
+	{ .pos = { .vec = { 0.5f, -0.5f, 0.5f } }, .normal = { .vec = { 1, 0, 0 } }, .uv = { .vec = { 0, 0 } } },
+	{ .pos = { .vec = { 0.5f, 0.5f, -0.5f } }, .normal = { .vec = { 1, 0, 0 } }, .uv = { .vec = { 1, 1 } } },
+	{ .pos = { .vec = { 0.5f, 0.5f, 0.5f } }, .normal = { .vec = { 1, 0, 0 } }, .uv = { .vec = { 0, 1 } } },
+
+	{ .pos = { .vec = { -0.5f, -0.5f, 0.5f } }, .normal = { .vec = { -1, 0, 0 } }, .uv = { .vec = { 1, 0 } } },
+	{ .pos = { .vec = { -0.5f, -0.5f, -0.5f } }, .normal = { .vec = { -1, 0, 0 } }, .uv = { .vec = { 0, 0 } } },
+	{ .pos = { .vec = { -0.5f, 0.5f, 0.5f } }, .normal = { .vec = { -1, 0, 0 } }, .uv = { .vec = { 1, 1 } } },
+	{ .pos = { .vec = { -0.5f, 0.5f, -0.5f } }, .normal = { .vec = { -1, 0, 0 } }, .uv = { .vec = { 0, 1 } } },
 };
 
 static vindex_t cube_indices[] = {
 	0, 1, 2, 2, 1, 3,
 	5, 4, 7, 7, 4, 6,
-	1, 5, 3, 3, 5, 7,
-	4, 0, 6, 6, 0, 2,
 	12, 13, 14, 14, 13, 15,
-	8, 9, 10, 10, 9, 11
+	8, 9, 10, 10, 9, 11,
+	16, 17, 18, 18, 17, 19,
+	20, 21, 22, 22, 21, 23
 };
 
 // -------------------------------------------------------------------------------------------------
